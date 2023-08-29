8866a7585d64f0966f6dcc3bec66c04a4499ce90
Rony Lee <rongyil33@gmail.com>
2023-08-29 16:26:06 +0800

    Add ejs-mate for boilerplate

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const ejs = require("ejs");
+const ejsMate = require("ejs-mate");
 const path = require("path");
 
 const app = express();
 
+app.engine("ejs", ejsMate); // use ejs-mate for layout
 app.set("view engine", "ejs"); // use .ejs files for frontend
 app.use(express.static(path.join(__dirname, "public"))); // connect css & js files in /public
 
