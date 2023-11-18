9947437e3796353264dc1fea991795cba74ca6ab
JiQian-K <kelvinfruits@gmail.com>
2023-11-18 15:56:51 +0800

    Testing login
    Just try some login functions

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,7 @@
-alert("Hello World!");
+//alert("Hello World!");
+
+function Login(){
+    var username = document.getElementById("txtUsername").value;
+    var password = document.getElementById("txtPassword").value;
+    alert(username + " :: " + password);
+}
