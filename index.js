d32fa0bbea3469b8b8eb89917b79b1d57baa88fc
Rony Lee <rongyil33@gmail.com>
2024-01-15 22:42:44 +0800

    Connect controller in route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const ejsMate = require("ejs-mate");
 const path = require("path");
+const feed = require("./controllers/feed");
 
 const app = express();
 
@@ -9,10 +10,8 @@ app.engine("ejs", ejsMate); // use ejs-mate for layout
 app.set("view engine", "ejs"); // use .ejs files for frontend
 app.use(express.static(path.join(__dirname, "public"))); // connect css & js files in /public
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.get("/", feed.home);
 
 app.listen(3000, () => {
-  console.log("App is running!");
+  console.log("App is running on 3000!");
 });
