f156346bf84b80d9b62e1e46dcccef3ffce1cb62
Rony Lee <rongyil33@gmail.com>
2024-03-07 00:35:22 +0800

    Fix feeds and user not connected in seed sample

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config(); // load .env
 const mongoose = require("mongoose");
 const Feed = require("../models/feed");
+const User = require("../models/user");
 const faker = require("faker");
 
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/Meddit";
@@ -14,9 +15,14 @@ db.once("open", function () {
 
 const seedDB = async () => {
   await Feed.deleteMany({});
+  await User.deleteMany({});
+
+  const user = new User({ username: "admin", email: "admin@test.com" });
+  await User.register(user, "admin");
 
   for (i = 0; i < 10; i++) {
     const feed = new Feed({
+      author: user._id,
       title: faker.random.words(2),
       description: faker.hacker.phrase(),
     });
