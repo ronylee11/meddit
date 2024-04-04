585289fed8362b7fe30de7a04e7f9e8bfa0031fe
Rony Lee <rongyil33@gmail.com>
2024-04-04 10:06:16 +0800

    Add client side navigation to login page for comment button, show page

---
+++ public/javascripts/showPage.js
@@ -0,0 +1,6 @@
+const commentButton = document.querySelector("#commentButton");
+
+commentButton.addEventListener("click", function() {
+    // navigate to /login
+    location.href = "/login";
+});
